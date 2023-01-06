2bb3d15ec3458e6a1bae292363ca4a72c28947fa
Tian Wang <50165004+tianwang-wow@users.noreply.github.com>
2023-01-06 13:12:25 -0500

    Add files via upload

--- func.R
+++ func.R
@@ -1,46 +1,29 @@
+Rcpp::sourceCpp('func_cpp.cpp')
+
 reweighted_RF = function(RF, # a fitted random forest
                          data_x, # a matrix of input features: row: subjects; column: features
-                         data_y, # a vector of responses: either continuous or binary class
-                         Kmat_ls, # a list of kernel matrices (not needed if pred = F)
-                         pred = F) # whether to find predictions on training samples
+                         data_y # a vector of responses: either continuous or binary
+)
 {
-  if (is.factor(data_y)) { data_y = as.numeric(as.character(data_y)) }
-  n_sam = RF$num.samples # number of all samples
-  inbag_counts = matrix(unlist(RF$inbag.counts), nrow = n_sam, byrow = FALSE) # row: subject; column: tree;
+  n_sam = RF$num.samples
+  inbag_counts = matrix(unlist(RF$inbag.counts), nrow = n_sam, byrow = FALSE)
   terminal_nodes = predict(object = RF, data = data.frame(data_x), type = 'terminalNodes', predict.all = TRUE)$predictions
   
-  pred_mat = NULL
-  if (pred) { # if we want to prediction on TR samples
-    weight_counts = OOB_weight_cts(inbag_counts, terminal_nodes)
-    Kmat_RF = weight_counts / rowSums(weight_counts) # kernel from RF
-    pred_mat = Kmat_RF %*% data_y # predicted prob on TR set using RF
-    colnames(pred_mat)[1] = 'Original' # record this prediction prob
-    
-    n_K = length(Kmat_ls) # number of distance matrices
-    for (l_id in 1:n_K) { # for each kernel
-      Kmat = Kmat_ls[[l_id]] # current Kmat
-      temp_weight = Kmat * Kmat_RF # weight matrix for prediction
-      temp_weight = temp_weight / rowSums(temp_weight) # scale the weight matrix
-      temp_predictions = temp_weight %*% data_y # predicted prob in TR set
-      pred_mat = cbind(pred_mat, temp_predictions) # cbind predcited prob
-    }
-    colnames(pred_mat)[-1] = names(Kmat_ls) # add method names
-  }
-  
   out = list(RF = RF,
-             inbag_counts = inbag_counts, # row: subject; column: tree;
-             terminal_nodes = terminal_nodes,
+             inbag_counts = inbag_counts, # inbag counts
+             terminal_nodes = terminal_nodes, # terminal nodes
              data_x = data_x,
-             data_y = data_y,
-             pred_mat = pred_mat)
+             data_y = data_y)
   return(out)
 }
 
 
 predict_reweighted_RF = function(fit, # a fitted reweighted_RF
-                                 new_data_x, # a matrix of input features: row: subjects; column: features
-                                 new_Kmat_ls) # a list of kernel values: row TE x column TR samples
+                                 new_data_x, # a matrix of input features: row: new subjects; column: features
+                                 new_Kmat_ls # a list of kernel values: row TE x column TR samples
+)
 {
+  if (is.factor(fit$data_y)) { data_y = as.numeric(as.character(fit$data_y)) }
   new_terminal_nodes = predict(object = fit$RF,
                                data = data.frame(new_data_x), 
                                type = 'terminalNodes', 
@@ -51,17 +34,16 @@ predict_reweighted_RF = function(fit, # a fitted reweighted_RF
   Kmat_RF = new_wt / rowSums(new_wt) # kernel from RF
   
   pred_mat = NULL
-  pred_mat = Kmat_RF %*% fit$data_y # predicted prob on new data set using RF
-  colnames(pred_mat)[1] = 'Original' # record this prediction prob
-  n_K = length(new_Kmat_ls) # number of kernels
-  for (l_id in 1:n_K) { # for each kernel
-    new_Kmat = new_Kmat_ls[[l_id]] # current Kmat: row TE x column TR
-    temp_weight = new_Kmat * Kmat_RF # weight matrix for prediction
-    temp_weight = temp_weight / rowSums(temp_weight) # scale the weight matrix
-    temp_predictions = temp_weight %*% fit$data_y # predicted prob in TR set
-    pred_mat = cbind(pred_mat, temp_predictions) # cbind predcited prob
+  pred_mat = Kmat_RF %*% fit$data_y
+  colnames(pred_mat)[1] = 'Original'
+  for (l_id in 1:length(new_Kmat_ls)) {
+    new_Kmat = new_Kmat_ls[[l_id]]
+    temp_weight = new_Kmat * Kmat_RF
+    temp_weight = temp_weight / rowSums(temp_weight)
+    temp_predictions = temp_weight %*% fit$data_y
+    pred_mat = cbind(pred_mat, temp_predictions)
   }
-  colnames(pred_mat)[-1] = names(new_Kmat_ls) # add method names
+  colnames(pred_mat)[-1] = names(new_Kmat_ls)
   
   out = list(pred_mat = pred_mat)
   
--- func_cpp.cpp
+++ func_cpp.cpp
@@ -4,50 +4,23 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 
-
-// [[Rcpp::export()]] //OOB weight counts used in 'PhyEnRF_fit'
-arma::mat OOB_weight_cts (arma::mat inbag_counts,
-                          arma::mat terminal_nodes) {
-  int n_sub = inbag_counts.n_rows; // number of subjects
-  arma::uvec RFindex, sampleids, i_vec, j_vec; int j;
-  arma::mat weight_counts(n_sub, n_sub); weight_counts.fill(0); // initialize distances to 0
-  for (int i=0; i<=(n_sub-1); i++) {
-    i_vec = i;
-    RFindex = arma::find(inbag_counts.row(i) == 0); //which trees such that i-th sample in data_x is not used
-    if (RFindex.n_elem >= 1) { // 09/29/2021: Add this "if"
-      for (int k=0; k<=(RFindex.n_elem-1); k++) {
-        j_vec = RFindex(k); j = RFindex(k);
-        sampleids = arma::find(terminal_nodes.col(j) == terminal_nodes(i,j)); // contributed old sample ids to predict the i-th newsample
-        weight_counts.submat(i_vec, sampleids) = weight_counts.submat(i_vec, sampleids) +
-          trans(inbag_counts.submat(sampleids, j_vec) / arma::accu(inbag_counts.submat(sampleids, j_vec)));
-      }
-    }
-  }
-  return weight_counts;
-}
-
-
-// [[Rcpp::export()]] //OOB weight counts used in 'PhyEnRF_fit'
+// [[Rcpp::export()]]
 arma::mat new_weight_cts (arma::mat inbag_counts,
                           arma::mat terminal_nodes,
                           arma::mat new_terminal_nodes) {
-  int n_old = terminal_nodes.n_rows; // number of old subjects
-  int n_new = new_terminal_nodes.n_rows; // number of new subjects
+  int n_old = terminal_nodes.n_rows;
+  int n_new = new_terminal_nodes.n_rows;
 
   arma::mat new_wt_cts(n_new, n_old); new_wt_cts.fill(0);
   arma::uvec sampleids, i_vec, k_vec;
-  // int n_tree = terminal_nodes.n_cols;
   for (int i=0; i<=(n_new-1); i++) {
     i_vec = i;
-    for (int k=0; k<=(terminal_nodes.n_cols-1); k++) { //for each tree
+    for (int k=0; k<=(terminal_nodes.n_cols-1); k++) {
       k_vec = k;
-      sampleids = arma::find(terminal_nodes.col(k) == new_terminal_nodes(i,k));//contributed old sample ids to predict the i-th newsample
+      sampleids = arma::find(terminal_nodes.col(k) == new_terminal_nodes(i,k));
       new_wt_cts.submat(i_vec, sampleids) = new_wt_cts.submat(i_vec, sampleids) +
         trans(inbag_counts.submat(sampleids, k_vec) / arma::accu(inbag_counts.submat(sampleids, k_vec)));
     }
-  } //note: sum(weight_counts[1,]) == n_tree
+  }
   return new_wt_cts;
 }
-
-
-
